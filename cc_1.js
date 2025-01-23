95793ea95f36c4f27fda6bd90dee8c255293679b
lucas0walters <lucas.o.walters@gmail.com>
2025-01-23 12:34:14 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+//Task 1 - Employee Information
+let employeeName = "Employee Name"
+const employeeID = 1
+var isActive = true
+console.log(`employeeName = ${employeeName}\nType is: ${typeof(employeeName)}\n
+employeeID = ${employeeID}\nType is: ${typeof(employeeID)}\n
+isActive = ${isActive}\nType is: ${typeof(isActive)}`)
